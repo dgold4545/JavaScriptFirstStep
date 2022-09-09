debe225036f5a487474a76868881f067f2947188
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-09 21:47:13 +0300

    ex_#_10

---
+++ book/09.09.22.(10)/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// let a = 0.5;
+// console.log(a);
+
+// let a = 0.5 + 0.5;
+// console.log(a);
+
+// let a = 1.5 + .5;
+// console.log(a);
+
+
+let a = 1.5;
+let b = 0.75;
+let summ = a + b;
+console.log(summ);
