62571183f6614f06278750d810052a1522a6e2ce
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-09 21:04:34 +0300

    ex_#_8

---
+++ book/09.09.22.(8)/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// let a = 1;
+
+// let b = 2;
+
+// alert(a + b);
+
+// let a = 1;
+// let b = 2;
+// let c = a + b;
+
+// alert(c);
+// console.log(" c = a + b ", c);
+
+// let a = 10;
+// let b = 2;
+
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(a / b);
+// alert(a % b);
+
+// let c = 10;
+// let d = 5;
+// let result = c + d;
+// alert(result);
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// alert(a + b + c);
+
+let a = 10;
+let b = 5;
+let c = a - b;
+
+let d = 7;
+
+let result = c + d;
+alert(result);
