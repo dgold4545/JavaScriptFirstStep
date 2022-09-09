a14f4d7e020bfc300b4e4ed85489e2ede9523afc
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-09 22:10:22 +0300

    ex_#_13

---
+++ book/09.09.22.(13)/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// console.log(10 ** 3);
+
+// let a = 10;
+// console.log(a ** 3);
+
+// let a = 10;
+// let b = 3;
+
+// console.log(a ** b);
+
+// console.log(2 ** 10);
+
+// console.log(2 * 2 ** 3);
+
+// console.log((3 * 2) ** 3);
+
+// console.log( 3 * 2 ** (3 + 1));
+
+// console.log( 2 ** 3 * 3);
+
+console.log( 3 * 2 ** 3 * 3);
