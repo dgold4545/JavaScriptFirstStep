f6ab14c5b9f746194157b952102efd2e6993ecb3
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 20:16:26 +0300

    ex_#_32

---
+++ book/09.10.22.(32)/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let a = 0.1 + 0.2;
+console.log(a);
+
+console.log(a.toFixed(2));
+
+console.log(+a.toFixed(2));
+
+console.log(Number(a.toFixed(2)));
+
+console.log(0.1 * 0.2);
+
+console.log(0.3 - 0.1);
+
+let b = 0.1 * 0.2;
+
+let c = 0.3 - 0.1;
+
+console.log(Number(b.toFixed(2)));
+
+console.log(Number(c.toFixed(1)));
