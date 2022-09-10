27e181bdd69b01a1cab3ac0fdb7735cd60872cb1
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 14:01:09 +0300

    ex_#_21

---
+++ book/09.10.22.(21)/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let a = '123';
+
+console.log('2' * '3');
+
+console.log('2' * '3s');
+
+
+console.log('1' + '2');
+
+console.log(1 + '2');
+
+
+console.log( '1' + (2 + 3));
+
+console.log( '1' + 2 + 3);
+
+console.log( 2 + 3 + '1');
+
+console.log('5' + '2');
+console.log(5 + '2');
+console.log(5 + 2);
+console.log('5' * '2');
+console.log('5' - '2');
+console.log('5' / '2');
+console.log('5' % '2');
+console.log('5s' * '2');
+console.log('5s' + '2');
+console.log((-'5') + (-'2'));
+console.log('5' + '2');
+console.log('5' * 1 + '2' * 1);
+console.log('5' * '1' + '2' * '1');
+console.log( '' + 3 + 1);
