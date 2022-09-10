69fb0ce33a1f634d449d60e321a3256f5235da1b
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 12:01:49 +0300

    ex_#_15

---
+++ book/09.10.22.(15)/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let str = 'abc' + 'def';
+console.log(str);
+
+let str1 = 'abc';
+let str2 = 'def';
+
+console.log(str1 + str2);
+
+
+let str3 = 'abc';
+let str4 = 'def';
+
+console.log(str3 + '!!!' + str4);
+
+console.log(str3 + ' ' + str4);
+
+console.log(str2 + ' ' + 'def*ck');
+
+console.log(str3 + ' def');
+
+let str5 = '!!!';
+console.log(str5);
+
+let str6 = 'java';
+let str7 = 'script';
+
+console.log(str6 + str7);
+
+let str8 = 'hello';
+let str9 = 'world';
+console.log(str8 + ' ' + str9);
+console.log(str8 + ' ' + str9);
