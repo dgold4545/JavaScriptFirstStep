4711b2a51d7b1c898a4c15a97d1484b6fa8e51b0
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 19:38:51 +0300

    ex_#_30

---
+++ book/09.10.22.(30)/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let num = 1;
+num = num + 2;
+console.log(num);
+
+let num1 = 1;
+num1 = num1 + 1;
+num1 = num1 + 1;
+console.log(num1);
+
+let num2 = 1;
+num2 = num2 + 2;
+num2 = num2 + 3;
+
+console.log(num2);
+
+let num3 = 1;
+num3 += 3;
+console.log(num3);
+
+let num4 = 2;
+num4 -= 3;
+console.log(num4);
+
+let num5 = 2;
+num5 *= 3;
+console.log(num5);
+
+let num6 = 3;
+num6 /= 3;
+console.log(num6);
+
+let num7 = 47;
+console.log(num7);
+num7 += 7;
+console.log(num7);
+num7 -= 18;
+console.log(num7);
+num7 *= 10;
+console.log(num7);
+num7 /= 15;
+console.log(num7);
