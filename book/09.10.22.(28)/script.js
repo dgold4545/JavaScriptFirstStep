e5ef721f2825251d36f47c59f5466809e2722e7b
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 18:35:01 +0300

    ex_#_28

---
+++ book/09.10.22.(28)/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let str = 'abcde';
+console.log(str.length);
+let last = str.length - 1;
+console.log(last);
+
+console.log(str[last]);
+
+let userName = 'dima1';
+console.log(userName.length);
+console.log(userName[userName.length - 1]);
+
+let lastName = 'Holovskiy';
+console.log(lastName.length);
+console.log(lastName[lastName.length - 1]);
+
+console.log(lastName[lastName.length - 2]);
+console.log(lastName[lastName.length - 3]);
