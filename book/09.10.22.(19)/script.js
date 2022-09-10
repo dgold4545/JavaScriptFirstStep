6e53beee7584f8b383e8a747d079a7c55d6cda3f
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 13:35:51 +0300

    ex_#_19

---
+++ book/09.10.22.(19)/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+console.log(123);
+
+let num = 213;
+console.log(num);
+
+
+let num1 = 321;
+let num2 = 456;
+
+console.log(num1);
+console.log(num2);
+
+let num3 = 'go';
+console.log(num1, num2);
+console.log(num, num3);
+
+
+let num4 = 'go ahead bro 1 ml';
+console.log(num4);
+
+
+console.log(num, num1, num2);
+console.log(num, num, num);
+
+
+console.log(123);
+console.log('123');
+
+console.log(num, num1, num2, num3, num4);
+
+console.log(123);
+console.log('Dmytro');
+console.log(true);
+console.log(false);
+console.log(Boolean);
+console.log(NaN);
+console.log(undefined);
+console.log(null);
+console.log(Infinity);
+console.log(-Infinity);
+
+
+// console.log(eee);
