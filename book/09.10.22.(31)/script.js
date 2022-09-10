e461827e0e1ca06ddd19591bb7e442d5ff3ebe36
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 20:05:52 +0300

    ex_#_31

---
+++ book/09.10.22.(31)/script.js
@@ -0,0 +1,86 @@
+'use strict';
+
+let num = 0;
+num += 1;
+console.log(num);
+
+let num1 = 0;
+num1++;
+console.log(num1);
+
+let num2 = 0;
+num2--;
+console.log(num2);
+
+let num3 = 10;
+console.log(num3);
+num3++;
+console.log(num3);
+num3++;
+console.log(num3);
+num3--;
+console.log(num3);
+
+let num5 = 0;
+console.log(num5);
+console.log(num5++);
+console.log(num5);
+
+let num6 = 0;
+console.log(num6);
+console.log(++num6);
+console.log(num6);
+
+let nu1 = 0;
+console.log(nu1);
+let nu2 = nu1++;
+console.log(nu2);
+console.log(nu1);
+
+let nu = 0;
+++nu;
+nu++;
+console.log(nu);
+
+let num11 = 3;
+alert(++num11);
+
+let num12 = 3;
+alert(num12++);
+alert(num12++);
+
+let num13 = 3;
+alert(--num13);
+
+let num14 = 3;
+alert(num14--);
+
+let num111 = 3;
+let num222 = ++num111;
+alert(num111);
+alert(num222);
+
+let num1111 = 3;
+let num2222 = num1111++;
+alert(num1111);
+alert(num2222);
+
+let num11 = 3;
+let num22 = --num11;
+alert(num11);
+alert(num22);
+
+let num01 = 3;
+let num02 = num01--;
+alert(num01);
+alert(num02);
+
+let num01 = 3;
+
+num01++;
+
+let num02 = num01--;
+
+alert(num01++);
+
+alert(--num02);
