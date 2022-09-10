059b1dfd75548a8be1d295237da809e2e8b5b2e7
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 17:19:44 +0300

    ex_#_25

---
+++ book/09.10.22.(25)/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log(String(true));
+console.log(String(false));
+
+console.log(true);
+console.log(false);
+
+console.log('a' + true);
+
+// console.log(1 + true);
+
+console.log(Number(true));
+console.log(Number(false));
+
+console.log(true + 1);
+console.log(true + true);
+
+alert(true + 3);
+alert(true + true);
+alert(true - true);
+alert(true + false);
+
+alert('1' + true);
+alert(String(true) + 1);
+alert(String(true) + Number(true));
