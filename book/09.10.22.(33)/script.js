8d5dec0acd3dc6390b8208317fa55354d920c2dd
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 20:54:34 +0300

    ex_#_33

---
+++ book/09.10.22.(33)/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// let userName = prompt('what is your name?');
+// console.log(`your name is ${userName}`);
+// alert(`your name is ${userName}`);
+
+// let userAge = prompt('what is your age?');
+// alert(`Your age is ${userAge}`);
+// console.log(userAge);
+
+// let num1 = prompt('enter the first number');
+// let num2 = prompt('enter the second number');
+
+// alert(num1 + num2);
+
+let firstNumber = Number(prompt('enter the first Number'));
+let secondNumber = Number(prompt('enter the second Number'));
+
+alert(firstNumber + secondNumber);
+console.log(firstNumber + secondNumber);
+
+let square = Number(prompt('enter the square data'));
+
+console.log(square ** 2);
+
+let sideOne = Number(prompt('enter side one'));
+let sideTwo = Number(prompt('enter side two'));
+let resultSide = sideOne * sideTwo * 2;
+console.log(sideOne * sideTwo * 2);
+alert(resultSide);
