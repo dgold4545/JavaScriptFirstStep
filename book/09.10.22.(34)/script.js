2f92b49a3ecede9ff725c884fbf833de024ea902
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 21:14:32 +0300

    ex_#_34

---
+++ book/09.10.22.(34)/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+document.write(123);
+
+document.write('text');
+
+document.write('<b>text</b>');
+
+let str = 'text';
+document.write(str);
+
+document.write('<b>' + str + '</b>');
+
+document.write('<br> text <br> text');
+
+document.write('<br> text1 <br>');
+document.write('text2 <br>');
+document.write('text3 <br>');
+
+document.write('text');
+document.write('<br>');
+document.write('text');
+
+document.write('<br> <br>');
+document.write('my name <b>Dima</b>');
+
+document.write(' <i>Holovskyi</i>');
+
+let userName = 'firstName';
+document.write(' <i>' + userName + '</i>');
+document.write('<br>');
+
+document.write(`1` + '<br>');
+document.write(`2` + '<br>');
+document.write(`3` + '<br>');
+document.write(`4` + '<br>');
+document.write(`5` + '<br>');
