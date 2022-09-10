ef1afd9998a5ca15eb1e65ccc0364e5cf2934dee
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 21:45:45 +0300

    ex_#_35

---
+++ book/09.10.22.(35)/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+let num1 = 1;
+let num2 = 2;
+console.log('сумма: ' + num1 + num2);
+
+let a = 1;
+let b = 2;
+
+console.log(a + b);
+
+let num = '123';
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+console.log(sum);
+
+let num11 = 123;
+num11 = String(num11);
+console.log(num11[0]);
+
+let a1 = 0;
+console.log(++a1);
+
+let num22 = 123;
+num22 = String(num22);
+console.log(num22.length);
+
+let a22 = 24 * 60 * 60;
+console.log(a22);
+
+let num33 = 123;
+let str = String(num);
+console.log(num.length);
+
+let num44 = 123;
+let str44 = String(num44);
+console.log(str44[str44.length - 1]);
+
+let nu = 123;
+let st = String(nu);
+console.log(st[st.length - 1]);
+
+let num55 = 123;
+let str55 = String(num55);
+console.log(str55.length);
+
+let num66 = 123;
+let str66 = String(num66);
+console.log(str66[2]);
+console.log(str66.length);
+
+let a77 = '123';
+let b77 = '456';
+let s = Number(a77) + Number(b77);
+
+console.log(s);
+
+let aaa88 = 1;
+let bbb88 = 2;
+let ccc88 = 3;
+
+console.log(aaa88 + bbb88 + ccc88);
