49b46aec7451f2c3b5ff5dec7aca69f7230b73cb
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 18:24:21 +0300

    ex_#_27

---
+++ book/09.10.22.(27)/script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+let str = 'abcde';
+
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
+
+let fin = 'dima';
+console.log(fin.length);
+console.log(fin[0]);
+console.log(fin[1]);
+console.log(fin[3]);
+
+let str1 = 'abcde';
+let num = 3;
+
+console.log(str1[num]);
+console.log(Boolean(''));
+
+let str2 = 'abcde';
+console.log(str2.length);
+console.log(str2[0]);
+
+console.log(str2[2]);
+console.log(str2[4]);
+
+console.log(Boolean(NaN));
+
+let str3 = 'abcde';
+console.log(str3.length);
+
+let str31 = str3[4] + str3[3] + str3[2] + str3[1] + str3[0];
+console.log(str31);
+// let str01 = str3[0];
+// console.log(str01);
+
+console.log(str3[2]);
+let str01 = str3[0];
+console.log(str01);
+
+console.log(Boolean(''));
+console.log(str3.length);
+console.log(str3[4], str3[3], str3[2], str3[1], str3[0]);
+let str02 = str3[4];
+console.log(str02);
+let str03 = str3[3];
+console.log(str03);
+let str04 = str3[2];
+console.log(str04);
+let str05 = str3[1];
+console.log(str05);
+let str06 = str3[0];
+console.log(str06);
+
+let str11 = str02 + str03 + str04 + str05 + str06;
+console.log(str11);
+
+let str77 = 'abcde';
+let str88 = 2;
+
+console.log(str77[str88]);
+
+// let str99 = 'abcde';
+// str99[0] = '!';
