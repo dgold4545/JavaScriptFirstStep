c03e90ba9a31b7536ea1f61699389858d736b663
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 12:28:18 +0300

    ex_#_17

---
+++ book/09.10.22.(17)/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let str = `abc`;
+console.log(str);
+
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt = `aaa + ${str1} + bbb + ${str2} + ccc`;
+console.log(txt);
+
+let str3 = 
+`a,
+b,
+c,`;
+console.log(str3);
