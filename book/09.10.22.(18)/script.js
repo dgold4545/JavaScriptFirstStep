fb2b39f94be3d022e46a64cd0ab606076606ef2f
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 12:39:50 +0300

    ex_#_18

---
+++ book/09.10.22.(18)/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let a;
+console.log(a);
+
+a = null;
+console.log(a);
+
+
+let c = true;
+let b = false;
+
+let d = `true`;
+let e = `false`;
+
+
+console.log(c);
+console.log(b);
+
+console.log(d);
+console.log(e);
+
+let f = 3;
+console.log(f);
+
+let h = 'Dmytro';
+let j = 'Holovskyi';
+console.log (h * j);
+
+
+console.log(1 / 0);
+console.log(-1 / 0);
+
+console.log(10 / 0);
+console.log (-10 / 0);
