52d3107a29c197db5bdb79919d776ba35749db7c
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 21:56:39 +0300

    ex_#_36

---
+++ book/09.10.22.(36)/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+document.write('go go go');
+
+console.log(24 * 60 * 60);
+console.log(24 * 60 * 60 * 30);
+console.log(24 * 60 * 60 * 365);
+console.log(24 * 60);
+console.log(24 * 60 * 365);
+console.log(1 * 1000);
+console.log(1 * 1_000_000);
+console.log(1 * 10_000_000);
+console.log(1 * 10 ** 12);
+console.log(1 * 10 ** 9);
