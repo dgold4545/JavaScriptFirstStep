3b62813c2e50934281233907b3170a119935e377
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 15:32:34 +0300

    ex_#_22

---
+++ book/09.10.22.(22)/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let a = '2';
+let b = '3';
+
+console.log(Number(a) + Number(b));
+
+let v = Number('2');
+let d = Number('3');
+
+console.log(v + d);
+
+let qw = '10';
+let wq = '20';
+
+console.log(Number(qw) + Number(wq));
+Number(console.log(qw + wq));
+
+// alert(Number('2') + Number('3'));
+
+// alert(2 + Number('3'));
+// alert('2' + Number('3'));
+
+let sa = +'2';
+let as = +'3';
+console.log(sa + as);
+
+console.log(Number('2s'));
+console.log(+'2s');
