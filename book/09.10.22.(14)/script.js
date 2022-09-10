d17558ffa82940895b1b146df59f4c286a87f0d5
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 11:45:23 +0300

    ex_#_14

---
+++ book/09.10.22.(14)/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// let str = "abc";
+// console.log(str);
+
+// let str = 'cba';
+// console.log(str);
+
+const FIRST_NAME = 'Dima';
+const LAST_NAME = 'Golovskyy';
+
+
+console.log(`${FIRST_NAME} ${LAST_NAME}`);
+console.log(FIRST_NAME + ' ' + LAST_NAME);
