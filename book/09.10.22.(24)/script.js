91469ba2a75840ff1cd21ac425e8fe49457066da
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-10 15:59:36 +0300

    ex_#_24

---
+++ book/09.10.22.(24)/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let str = String(123);
+console.log(str);
+
+let num1 = 1;
+let num2 = 2;
+
+console.log(String(num1) + String(num2));
+
+let myAge = 33;
+let yourAge = 21;
+
+console.log(String(myAge) + String(yourAge));
+
+let num = 12345;
+
+console.log(num.length);
+
+let str12 = String(num);
+console.log(str12.length);
+
+console.log(String(num).length);
+
+let str22 = 5304062;
+console.log(String(str22).length);
+
+let str11 = 1223344556;
+let str31 = 6554433221;
+
+let str41 = String(str11).length;
+console.log(str41);
+
+let str51 = String(str31).length;
+console.log(str51);
+
+console.log(str41 + str51);
