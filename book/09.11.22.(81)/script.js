229b3b8be4a2006d56b0ff62a21068a82b71cb15
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-11 21:54:42 +0300

    ex_#_80

---
+++ book/09.11.22.(81)/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let num = 3;
+
+if (num > 0 && num < 10) {
+    console.log('yes its true', num);
+} else {
+    console.log('fasle');
+}
+
+let num1 = 2;
+let num2 = 3;
+
+if (num1 == 2 && num2 == 3) {
+    console.log('true yes');
+} else {
+    console.log('wrong');
+}
+
+if (num > 0 && num < 5) {
+    console.log('oggggo ist true');
+} else {
+    console.log('nope ist false');
+}
+
+if (num >= 10 && num < 20) {
+    console.log('yes its true');
+} else {
+    console.log('nope you are sucks');
+}
+
+if (num1 >= 1 && num2 >= 3) {
+    console.log('true 100% true');
+} else {
+    console.log('nope you are full');
+}
