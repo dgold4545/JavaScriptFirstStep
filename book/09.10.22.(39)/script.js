70e58d8f8d758590bdf9405675037de6440a727b
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-11 17:27:54 +0300

    ex_#_39

---
+++ book/09.10.22.(39)/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let arr = ['a', 'b', 'c'];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+
+let arr1 = [1, 2, 3];
+
+console.log(arr1[0]);
+console.log(arr1[1]);
+console.log(arr1[2]);
+
+let arr2 = [1, 2, 3];
+
+console.log(arr2[2]);
+console.log(arr2[1]);
+console.log(arr2[0]);
+
+// document.write(arr2);
+
+let arr3 = [1, 2, 3];
+document.write(arr3[0]);
+document.write(arr3[1]);
+document.write(arr3[2]);
+document.write('<br> ! ');
+
+let arr4 = [1, 2, 3];
+console.log(arr4[0] + arr4[1] + arr4[2]);
+document.write(arr4[0] + arr4[1] + arr4[2]);
+
+document.write('<br>');
+
+let arr5 = ['a', 'b', 'c', 'd'];
+console.log(String(arr5[0]), String('+'), String(arr5[1]));
+console.log(arr5[0] + arr5[1]);
+console.log(`${arr5[0]}+${arr5[1]}+${arr5[2]}+${arr5[3]}`);
+
+document.write(`${arr5[0]}+${arr5[1]}+${arr5[2]}+${arr5[3]}`);
