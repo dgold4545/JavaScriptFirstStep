ec072b334b0b4c14e8447dce64b49145462523c1
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-11 23:32:04 +0300

    ex_#_89

---
+++ book/09.11.22.(90)/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// let test = 3;
+
+// if (Boolean(test) === true) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let test = '';
+
+// if (test) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let test = null;
+
+// if (test) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let test = false;
+
+// if (test) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+let test = -1;
+
+if (test) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
