af668706da22f2e1141b4835fe44f878007ff22e
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-11 17:02:28 +0300

    ex_#_38

---
+++ book/09.10.22.(38)/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// let arr = [];
+
+let arr = ['a', 'b', 'c'];
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 'a', 'b', null, true, false];
+
+let arr3 = ['dima', 'holovskiy', 28, '01', 198];
+alert(arr3);
+console.log(arr3);
+
+let arr4 = [5, 3, 0, 4, '  ', 0, 62];
+alert(arr4);
+console.log(arr4);
+
+let arr5 = [arr, 'dima', 'holovskiy'];
+alert(arr5);
+console.log(arr5);
