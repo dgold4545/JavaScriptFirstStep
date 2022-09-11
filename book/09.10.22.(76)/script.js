8340911910114ddd983b2871e09d2445aa42f44e
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-11 20:29:54 +0300

    ex_#_76

---
+++ book/09.10.22.(76)/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let test1 = 1;
+let test2 = 2;
+
+if (test1 < test2) {
+    console.log('test2 > test1');
+} else {
+    console.log('test1 =', test1);
+}
+
+if (test1 == test2) {
+    console.log('test1 == test2');
+} else {
+    console.log('test1 != test2');
+}
