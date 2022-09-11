bd18377d7eae8fb6793da76813b7547392e3103e
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-11 19:48:01 +0300

    ex_#_45

---
+++ book/09.10.22.(45)/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let arr = ['a', 'b', 'c'];
+console.log(arr[0]);
+
+let key = 0;
+console.log(arr[key]);
+
+let arr1 = ['a', 'b', 'c'];
+console.log(arr1);
+
+let key1 = 2;
+
+console.log(arr1[key1]);
+
+let arr3 = [1, 2, 3, 4, 5];
+let key2 = 1;
+let key3 = 2;
+
+let summ = arr3[key2] + arr3[key3];
+console.log(summ);
+
+arr3[9] = 13;
+console.log(arr3.length);
+console.log(arr3);
