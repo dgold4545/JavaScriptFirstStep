2825f2c716f8f78c5194263ef6f56fdedfddaf3b
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-11 22:00:49 +0300

    ex_#_82

---
+++ book/09.11.22.(82)/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// let num1 = -5;
+// let num2 = 15;
+
+// if (num1 > 0 || num2 > 0) {
+//     console.log('++++ dlf');
+// } else {
+//     console.log('false you full');
+// }
+let num = 2;
+
+// if (num == 0 || num == 1) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+if (num == 0 || num == 1 || num == 2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
