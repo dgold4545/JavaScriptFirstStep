9ee407340c0f2f8c228e19e8aafc2a8a3ab6ec2a
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-11 22:44:15 +0300

    ex_#_84

---
+++ book/09.11.22.(84)/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let num = 3;
+
+if ((num > 0 && num < 5) || (num > 10 && num < 20)) {
+    console.log(' yes its true');
+} else {
+    console.log('nope sheet');
+}
+
+if ((num > 0 && num < 5) || (num > 10 && num < 20)) {
+    console.log('yeeeeeeee');
+}
+
+let num1 = 3;
+let num2 = 5;
+
+if (num1 > 0 && (num2 == 3 || num2 == 4)) {
+    console.log('sasha true');
+} else {
+    console.log('dina false');
+}
+
+let num3 = 3;
+if ((num3 > 5 && num3 < 10) || num3 == 20) {
+    console.log('true da');
+} else {
+    console.log('false nooo');
+}
+
+let num4 = 3;
+
+if (num4 > 5 || (num4 > 0 && num4 < 3)) {
+    console.log('true daaaaaa');
+} else {
+    console.log('false nooooo');
+}
+
+let num5 = 3;
+
+if (num5 == 9 || (num5 > 10 && num5 < 20) || (num5 > 20 && num5 < 30)) {
+    console.log('true dsaaaaaa');
+} else {
+    console.log('false NOOOOO');
+}
