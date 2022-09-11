c4f44e00d0635488edcddd251169e8cc0a0ea041
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-11 21:41:05 +0300

    ex_#_80

---
+++ book/09.10.22.(80)/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+if (3 != 3) {
+    console.log('true +++');
+} else {
+    console.log('false');
+}
+
+if ('3' !== 3) {
+    console.log('true+++');
+} else {
+    console.log('false ____');
+}
+
+// let test1 = '3';
+// let test2 = '3';
+
+// if (test1 != test2) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let test1 = '3';
+// let test2 = '3';
+
+// if (test1 !== test2) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let test1 = 3;
+// let test2 = '3';
+
+// if (test1 != test2) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let test1 = 3;
+// let test2 = '3';
+
+// if (test1 !== test2) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+let test1 = 3;
+let test2 = 2;
+
+if (test1 !== test2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
