b9b7a9be749242fe6a8e5196594c429d3db941b6
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 12:42:06 +0300

    ex_#_99

---
+++ book/09.11.22.(99)/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+console.log(1 == 1);
+console.log(1 == 2);
+
+let a = 1;
+let b = 2;
+
+console.log(a == b);
+
+let result = a == b;
+console.log(result);
+
+a = 2 * (3 - 1);
+b = 6 - 2;
+
+console.log(a == b);
+
+a = 5 * (7 - 4);
+b = 1 + 2 + 7;
+
+console.log(a > b);
+console.log(a, b, a > b);
+
+a = 2 ** 4;
+b = 4 ** 2;
+
+console.log(a, b, a != b);
