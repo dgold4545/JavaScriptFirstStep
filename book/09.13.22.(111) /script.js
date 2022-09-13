bbecae0fc4847faa6624d658f2e6143414093b61
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 16:24:24 +0300

    ex_#_111

--- book/09.13.22.(111) /script.js
+++ book/09.13.22.(111) /script.js
@@ -1,3 +1,33 @@
 'use strict';
 
-let month = 12;
+for (let i = 1; i <= 9; i += 2) {
+    console.log(i);
+}
+
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+
+for (let i = 11; i < 34; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 101; i = i + 2) {
+    console.log(i);
+}
+
+for (let i = 0; i < 101; i += 5) {
+    console.log(i);
+}
+
+for (let i = 0; i < 100; i += 3) {
+    console.log(i);
+}
+
+for (let i = 100; i > 0; i -= 2) {
+    console.log(i);
+}
