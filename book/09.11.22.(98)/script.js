7b4e30b0c56cde8217dbbc3d3a6b9e8994a6e633
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 12:35:03 +0300

    ex_#_98

---
+++ book/09.11.22.(98)/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let age = 17;
+let adult = age >= 18 ? true : false;
+console.log(adult);
+
+if (age >= 18) {
+    adult = true;
+} else {
+    adult = false;
+}
+
+console.log(adult);
+
+let num = -1;
+let res = num >= 0 ? '1' : '2';
+console.log(res);
