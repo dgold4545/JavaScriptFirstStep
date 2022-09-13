7eb32ec16c690d6c7bf4a466151c0136e06a0854
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 20:24:22 +0300

    ex_#_114

--- book/09.13.22.(114)/script.js
+++ book/09.13.22.(114)/script.js
@@ -1,3 +1,45 @@
 'use strict';
 
-let month = 12;
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+let num = 500;
+
+while (num > 10) {
+    num = num / 2;
+    console.log(num);
+}
+console.log(num);
+let summ = num;
+console.log(summ);
+
+num = 500;
+while (num > 10) {
+    num /= 2;
+    console.log(num);
+}
+
+let num1 = 1;
+
+while (num1 <= 100) {
+    console.log(num1);
+    num1 += 1;
+}
+
+num1 = 11;
+
+while (num1 < 34) {
+    console.log(num1);
+    num1++;
+}
+
+num = 12;
+
+while (num < 1000) {
+    console.log(num);
+    num *= 3;
+}
