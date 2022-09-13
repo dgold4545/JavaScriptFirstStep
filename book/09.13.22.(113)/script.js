c87986384a81c79d3b137e7f62daf1edfe035ebf
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 20:05:22 +0300

    ex_#_113

--- book/09.13.22.(113)/script.js
+++ book/09.13.22.(113)/script.js
@@ -1,3 +1,55 @@
 'use strict';
 
-let month = 12;
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+};
+
+for (let key in obj) {
+    console.log(key);
+}
+
+for (let key in obj) {
+    console.log(obj[key]);
+}
+
+let objHome = {
+    x: 1,
+    y: 2,
+    z: 3,
+    numberFlet: 614,
+    numberFloor: 20,
+    squer: 73,
+};
+
+for (let key in objHome) {
+    console.log(key);
+}
+
+for (let dima in objHome) {
+    console.log(objHome[dima]);
+}
+
+let obj1 = {};
+obj1 = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+};
+
+console.log(obj1[1]);
+
+obj = {
+    x: 1,
+    y: 2,
+    z: 3,
+};
+
+for (let key in obj) {
+    console.log(key);
+}
+
+for (let keys in obj) {
+    console.log(obj[keys]);
+}
