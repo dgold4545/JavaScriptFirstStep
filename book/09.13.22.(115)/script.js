5648be2d8c1ff4cb5852d1eec9a93e86991b5e91
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 21:14:59 +0300

    ex_#_115

--- book/09.13.22.(115)/script.js
+++ book/09.13.22.(115)/script.js
@@ -1,3 +1,39 @@
 'use strict';
 
-let month = 12;
+let arr = [1, 2, 'vova', 4, 'dima'];
+
+for (let i = 0; i <= arr.length - 1; i++) {
+    console.log(arr[i]);
+}
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for (let i = 1; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+}
+
+arr = ['a1', 'b2', 'c3', 'd4', 'e5'];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for (let i = arr.length - 1; i > 0; i--) {
+    console.log(arr[i]);
+}
+
+for (let i = 1; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+arr = ['a', 'b', 'c', 'd', 'e'];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
