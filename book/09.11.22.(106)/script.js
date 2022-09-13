a3bf1b7c1be054590b5a29c95ea7399f15c00262
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 14:54:41 +0300

    ex_#_106

---
+++ book/09.11.22.(106)/script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let num = 12345;
+
+if (num[0] == 1) {
+    console.log('++++');
+} else {
+    console.log('_______arr2.length == 3');
+}
+
+num = 12345;
+console.log(num[0]);
+
+let str = String(num);
+
+if (str[0] == 1) {
+    console.log('number 1');
+} else {
+    console.log('nope');
+}
+
+num = 12345;
+
+if (String(num)[0] == 1) {
+    console.log('yes');
+} else {
+    console.log('nope');
+}
+
+console.log(String(num)[2]);
+
+num = 22345;
+
+if (String(num)[0] == 1 || String(num)[0] == 2) {
+    console.log('yes');
+} else {
+    console.log('nope');
+}
+
+let str1 = 991;
+
+if (String(str1)[2] == 0) {
+    console.log('yes my master');
+}
+
+num = 135;
+
+if (num % 2 == 0) {
+    console.log('da chetnoe');
+} else {
+    console.log('nope ne chetnoe');
+}
