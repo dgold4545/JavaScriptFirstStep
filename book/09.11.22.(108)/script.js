de18ea4a93b01b689825ac6ab5c37a3cc20d1fe3
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 15:34:04 +0300

    ex_#_108

---
+++ book/09.11.22.(108)/script.js
@@ -0,0 +1,122 @@
+'use strict';
+
+// let num1 = 1;
+// let num2 = 2;
+
+// if (num1 + num2 === 3) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num1 = 1;
+// let num2 = 2;
+
+// if (num1 + num2 === 3) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num1 = '1';
+// let num2 = '2';
+
+// if (+num1 + +num2 === 3) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num1 = '1';
+// let num2 = '2';
+
+// if (Number(num1) + Number(num2) === 3) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num = 123;
+
+// if (String(num)[0] == 1) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num = 123;
+
+// if (String(num)[0] == 1) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num = 123;
+
+// if (String(num)[0] === '1') {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num = 123;
+// let first = String(num)[0];
+
+// if (Number(first) === 1) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num = 12;
+
+// if (String(num).length === 2) {
+//     console.log('+++', String(num).length);
+// } else {
+//     console.log('---');
+// }
+
+// let num = 12;
+// let str = String(num);
+
+// if (str.length === 2) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num = 12;
+
+// if (String(num).length === 2) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num = 12;
+
+// if (String(num).length == String(2)) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+// let num = 12;
+
+// if (String(num).length === 2) {
+//     console.log('+++');
+// } else {
+//     console.log('---');
+// }
+
+let num = '123033';
+
+let sum1 = Number(num[0]) + Number(num[1]) + Number(num[2]);
+let sum2 = Number(num[3]) + Number(num[4]) + Number(num[5]);
+
+if (sum1 == sum2) {
+    console.log('суммы равны', sum1 == sum2, sum1 + sum2);
+} else {
+    console.log('суммы не равны', sum1);
+}
