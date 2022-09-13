811dea03da930cef26e7f371729eaf3e1c813f1e
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 12:17:50 +0300

    ex_#_96

---
+++ book/09.11.22.(96)/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// switch () {
+//     case: ;
+
+//     break;
+//     case:  ;
+
+//     break;
+
+//     default:
+
+//     break
+// }
+
+let num = 1;
+
+switch (num) {
+    case 1:
+        console.log('value1');
+        break;
+    case 2:
+        console.log('value2');
+        break;
+    case 3:
+        console.log('value3');
+    default:
+        console.log('incorrect valur');
+        break;
+}
+
+let lang = 'en';
+
+switch (lang) {
+    case 'ru':
+        console.log('ru ru ru');
+        break;
+    case 'en':
+        console.log(' en en en ');
+        break;
+    case 'de':
+        console.log('de de de ');
+        break;
+    default:
+        console.log('there is there are lan dont support');
+        break;
+}
