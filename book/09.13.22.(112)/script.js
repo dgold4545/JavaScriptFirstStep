fa452c278f7fc47265760b8fc292407fba512a0e
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 16:31:21 +0300

    ex_#_112

--- book/09.13.22.(112)/script.js
+++ book/09.13.22.(112)/script.js
@@ -1,3 +1,13 @@
 'use strict';
 
-let month = 12;
+let arr = [1, 2, 3, 4, 5];
+
+for (let elem of arr) {
+    console.log(elem);
+}
+
+let arr1 = ['a', 'b', 'c', 'd', 'e'];
+
+for (let dima of arr1) {
+    console.log(dima);
+}
