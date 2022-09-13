7b5cd9057d311c5ded0723b4b3ffde2d29edeffd
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-13 13:37:08 +0300

    ex_#_101

--- book/09.11.22.(101)/script.js
+++ book/09.11.22.(101)/script.js
@@ -7,11 +7,10 @@ if (true) {
 }
 console.log(res);
 
-// let num = 5;
+let num = 5;
 
-// if ( num >= 0) {
-
-// }
+if (num >= 0) {
+}
 
 let test = true;
 res;
@@ -33,3 +32,70 @@ if (age >= 18) {
 }
 
 console.log(adult);
+
+res = 1;
+
+if (true) {
+    res = 2;
+}
+
+console.log(res);
+
+if (true) {
+    let res = 2;
+    console.log(res);
+}
+
+console.log(res);
+
+let age = 17;
+let adult;
+
+if (age >= 18) {
+    adult = true;
+} else {
+    adult = 'nope';
+}
+
+console.log(adult);
+
+age = 17;
+adult;
+
+if (age >= 18) {
+    adult = true;
+} else {
+    adult = false;
+}
+
+console.log(adult);
+
+let age = 17;
+res;
+
+if (age >= 18) {
+    if (age <= 23) {
+        res = 'от 18 до 23';
+    } else {
+        res = 'больше 23';
+    }
+} else {
+    res = 'меньше 18';
+}
+
+console.log(res);
+
+let age = 33;
+let res;
+
+if (age >= 18) {
+    if (age <= 23) {
+        res = 'от 18 до 23';
+    } else {
+        res = 'больше 23';
+    }
+} else {
+    res = 'меньше 18';
+}
+
+console.log(res);
