6ae85fdbca24ca288b27797a42bc58352350e18d
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-14 14:49:41 +0300

    ex_#_50

---
+++ book/09.14.22.(50)/script.js
@@ -0,0 +1,79 @@
+'use strict';
+
+let obj = {};
+
+obj = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+};
+
+console.log(obj[1]);
+console.log(obj[2]);
+console.log(obj[3]);
+
+let daysOfWeek = {
+    1: 'monday',
+    2: 'tuesday',
+    3: 'wednesday',
+    4: 'thursdau',
+    5: 'friday',
+    6: 'saturday',
+    7: 'sunday',
+};
+
+console.log(daysOfWeek[3]);
+console.log(daysOfWeek[2]);
+console.log(daysOfWeek[7]);
+console.log(daysOfWeek[6]);
+console.log(daysOfWeek[1]);
+console.log(daysOfWeek[4]);
+console.log(daysOfWeek[5]);
+
+obj = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+};
+
+// alert(obj);
+
+console.log(obj);
+
+let month = {
+    1: 'january',
+    2: 'february',
+    3: 'march',
+    4: 'april',
+    5: 'may',
+    6: 'june',
+    7: 'july',
+    8: 'august',
+    9: 'september',
+    10: 'october',
+    11: 'november',
+    12: 'december',
+};
+
+console.log(month);
+console.log(month[5]);
+
+obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+};
+console.log(obj['b']);
+console.log(obj['a']);
+console.log(obj['b']);
+console.log(obj['c']);
+
+let user = {
+    currentName: 'dima',
+    surname: 'hold',
+    partanymic: 'anatoliyovich',
+};
+
+console.log(user['currentName'], user['surname'], user['partanymic']);
+
+console.log(user['surname'], user['currentName'], user['partanymic']);
