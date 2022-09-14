f1bf2bec183f5bf396e6f13d3a9339cda8944d18
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-14 12:16:31 +0300

    ex_#_48

---
+++ book/09.14.22.(48)/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let obj = {};
+
+obj = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+    4: 'focus',
+};
+
+console.log(obj);
+
+console.log(obj[0]);
+console.log(obj[1]);
+console.log(obj[2]);
+
+obj = {
+    dima: 'gold',
+    oly: 'pisiy',
+    opa: 'popa',
+};
+
+console.log(obj);
+console.log(obj['dima']);
+console.log(obj['opa']);
+
+obj = {
+    1: 'football',
+    2: 'doma',
+};
+
+console.log(obj[2]);
+
+let daysWeek = {
+    1: 'Monday',
+    2: 'Thusday',
+    3: 'Whenday',
+    4: 'thustau',
+    5: 'Friday',
+    6: 'Saturnday',
+    7: 'Sunday',
+};
+
+console.log(daysWeek);
+
+daysWeek = {
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+    7: 'Sunday',
+};
+console.log(daysWeek);
