fe6007f7c0367def67db873eedef65383762c336
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-14 14:59:44 +0300

    ex_#_51

---
+++ book/09.14.22.(51)/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+};
+
+console.log(obj.a);
+
+let date = {
+    year: 2022,
+    month: 'september',
+    day: 'wednesday',
+};
+
+console.log(date);
+
+console.log(date.year);
+console.log(date.month, date.day);
+console.log(date.year, date.month, date.day);
+console.log('-', date.year, '-', date.month, '-', date.day, '-');
