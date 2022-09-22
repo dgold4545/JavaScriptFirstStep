2c5fe84ce81f321184765360b963d39e4cfe1a76
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-22 22:52:14 +0300

    #40

---
+++ book/22.09.22.(40)/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let arr = ['a', 'b', 'c'];
+
+console.log(arr.length);
+
+arr;
+console.log(arr[arr.length - 1]);
+
+let dima = [1, 23, 47, 69, 103];
+
+console.log(dima[dima.length - 1]);
+
+console.log(dima.length);
+
+let sasha = [182, 'dina', null, false];
+console.log(sasha.length);
+
+console.log(sasha[sasha.length - 1], sasha[sasha.length - 2]);
+
+let natasha = [29, 162, 90];
+
+console.log(
+    'arr natasha',
+    natasha[natasha.length - 1],
+    natasha[natasha.length - 2],
+    natasha[natasha.length - 3]
+);
+
+console.log(natasha.length, natasha[natasha.length - 1]);
