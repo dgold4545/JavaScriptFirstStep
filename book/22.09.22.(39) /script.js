332c3a2b0f4c4f5ddb7adf6ff917e51f7eca2f8d
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-22 15:46:49 +0300

    #39

---
+++ book/22.09.22.(39) /script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let arr = ['a', 'b', 'c'];
+
+console.log(arr[0]);
+console.log(arr[3]);
+console.log(arr[2]);
+console.log(arr[0] + arr[1] + 'cdfg');
+let dima = [1, 2, 3];
+alert(dima[0]);
+alert(dima[1]);
+alert(dima[2]);
+
+console.log('(arr - dima)', dima);
+
+let sasha = [1, 2, 3];
+console.log(sasha[0] + sasha[1] + sasha[2]);
+alert(sasha[0] + sasha[1] + sasha[2]);
+
+let tany = ['a', 'b', 'c', 'd'];
+
+console.log(tany[0], '+', tany[1], '+', tany[2], '+', tany[3]);
+console.log(`'${tany[0]}+${tany[1]}+${tany[2]}+${tany[3]}'`);
