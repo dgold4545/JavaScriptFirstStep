bf459c2652e774e0a038c2f8201a7f1fa7867092
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-23 07:38:54 +0300

    #44

---
+++ book/23.09.22.(44)/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let arr = [];
+
+arr.push('a');
+arr.push('b');
+arr.push('c');
+
+arr[2] = 0;
+console.log(arr);
+
+let diam = [];
+diam.push(null);
+
+diam.push(1, 2, 3);
+console.log(diam);
+console.log(diam.length);
+let sasha = [1, 2, 3];
+sasha.push(4, 5);
+console.log(sasha, sasha.length);
