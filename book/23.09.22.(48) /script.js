d067c3f981639cce419543e7b9d6143d2032d844
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-23 09:32:26 +0300

    #48

---
+++ book/23.09.22.(48) /script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let obj = {};
+
+obj = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+};
+
+console.log(obj[1]);
+
+let dayOfTheWeek = {
+    1: 'monday',
+    2: 'tuesday',
+    3: 'wednesday',
+    4: 'thursday',
+    5: 'friday',
+    6: 'saturday',
+    7: 'sunday',
+};
+
+console.log(dayOfTheWeek);
+console.log(dayOfTheWeek[1]);
+console.log(dayOfTheWeek[2]);
+console.log(dayOfTheWeek[3]);
+console.log(dayOfTheWeek[4]);
+console.log(dayOfTheWeek[5]);
+console.log(dayOfTheWeek[6]);
+console.log(dayOfTheWeek[7]);
