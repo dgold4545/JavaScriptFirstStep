cdb3d385ecf8a18d68c50bf945a18fbf5288677c
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-23 08:16:21 +0300

    #47

---
+++ book/23.09.22.(47)/script.js
@@ -0,0 +1,10 @@
+'use strict';
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[arr.length - 1]);
+
+console.log(arr[1] + arr[2] + arr[3] + arr[4] + arr[0]);
+console.log(1 + 2 + 3 + 4 + 5 + 5 + 4 + 3 + 2 + 1);
+
+console.log(arr.length);
+
+console.log(arr.length);
