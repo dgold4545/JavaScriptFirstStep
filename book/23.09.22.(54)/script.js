d5fbfafa71f421ef5625a1539824a385a05f6529
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-23 12:16:09 +0300

    #54

---
+++ book/23.09.22.(54)/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let obj = {};
+
+obj['a'] = 1;
+obj['b'] = 2;
+obj['c'] = 3;
+
+console.log(obj);
+
+obj.dma = 'golov';
+obj.da = 'dima';
+obj.t = 'timur';
+console.log(obj);
+
+let pizza = {};
+console.log(pizza);
+pizza.taste = 'paperonni';
+pizza.size = 'xl';
+pizza.cost = 299;
+pizza.time = 30;
+
+console.log(pizza);
