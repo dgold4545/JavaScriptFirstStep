21a3f7e33d092994eb1da16674fcc93c916c2883
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-23 09:54:50 +0300

    #49

---
+++ book/23.09.22.(49)/script.js
@@ -0,0 +1,27 @@
+'use strict';
+let obj = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+};
+
+// alert(obj);
+console.log(obj);
+
+let monthName = {
+    1: 'january',
+    2: 'february',
+    3: 'march',
+    4: 'april',
+    5: 'may',
+    6: 'june',
+    7: 'july',
+    8: 'august',
+    9: 'september',
+    10: 'october',
+    11: 'november',
+    12: 'december',
+};
+
+console.log(monthName);
+console.log(monthName[2], monthName[4], monthName[9]);
