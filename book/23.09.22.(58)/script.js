f204c53ab48bc791a1bedf9c804b457026762d98
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-23 12:56:00 +0300

    #58

---
+++ book/23.09.22.(58)/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+};
+
+let key = 'a';
+
+console.log(obj[key]);
+
+let pizza = {
+    size: 'xl',
+    price: 299,
+    taste: 'paperonni',
+};
+console.log(pizza);
+
+let userChoise = 'size';
+console.log(userChoise);
+
+console.log(pizza[userChoise]);
