524bc867a1de7d0f4ffe29b0c36d7a759ec741a9
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-23 13:25:16 +0300

    #60

---
+++ book/23.09.22.(60)/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+};
+
+let key = 'a';
+
+console.log(obj[key]);
+
+let obj1 = {
+    x: 1,
+    y: 2,
+    z: 3,
+};
+
+let prop = 'x';
+console.log(obj1[prop]);
+
+let obj2 = {
+    x: 1,
+    y: 2,
+    z: 3,
+};
+
+let prop2 = 'x';
+console.log(obj2[prop2]);
