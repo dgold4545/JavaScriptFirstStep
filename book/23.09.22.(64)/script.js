a9081c8cecb72f27aa0b8a0917b3a21d71f5e9f0
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-23 13:52:03 +0300

    #64

---
+++ book/23.09.22.(64)/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log(typeof {});
+console.log(typeof { x: 1, y: 2, z: 3 });
+console.log(typeof {});
+let obj1 = { x: 1, y: 2, z: 3 };
+console.log(typeof obj1);
+let obj = { x: 1, y: 2, z: 3 };
+console.log(typeof obj['x']);
