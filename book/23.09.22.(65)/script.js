1f715267110495c3279e44b2b80f53749f3f9fc9
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-23 13:54:56 +0300

    #65

---
+++ book/23.09.22.(65)/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log(typeof []);
+console.log(typeof { x: 1, y: 2, z: 3 });
+console.log(typeof [1, 2, 3]);
+let arr = [1, 2, 3];
+console.log(typeof arr);
+arr = [1, 2, 3];
+console.log(typeof arr[0]);
+arr = ['1', '2', '3'];
+console.log(typeof arr[0]);
