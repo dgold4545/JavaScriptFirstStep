40ec38aa16d04e2e00352567e4612ec7dd6c224b
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-27 18:26:45 +0300

    ex83

---
+++ book/27.09.22.(83)/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let num = 3;
+
+if ((num > 0 && num < 5) || (num > 10 && num < 20)) {
+    console.log('+++++');
+} else {
+    console.log('====');
+}
+
+if (num == 0 || (num > 1 && num < 5)) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
