2f176baf6ff11d17598963acaa4b9632efd47a85
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-27 16:37:07 +0300

    ex77

---
+++ book/27.09.22.(77)/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let test = 'abc';
+
+if (test == 'abc') {
+    console.log('true eeeee');
+} else {
+    console.log('nope this is false');
+}
+
+let test1 = 'dima';
+let test2 = 'dima';
+
+if (test1 == test2) {
+    console.log('finish');
+} else {
+    console.log('nipe ');
+}
