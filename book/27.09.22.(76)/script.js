9780294075760bdae27ef3695849e6d30bdf595a
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-27 16:33:00 +0300

    ex74

---
+++ book/27.09.22.(76)/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let test1 = 1;
+let test2 = 2;
+
+if (test2 > test1) {
+    console.log('true eeee');
+} else {
+    console.log('noppppeeee');
+}
+
+if (test1 > test2) {
+    console.log(`${test1}`);
+} else {
+    console.log(test2, `test2 ${test2}`);
+}
+
+if (test1 == test2) {
+    console.log('test1 and test2 equal');
+} else {
+    console.log('not an equal');
+}
