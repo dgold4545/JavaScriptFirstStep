1c0af419cd14fc22ffe543b4ca6a7b10211517e9
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-27 16:41:10 +0300

    ex78

---
+++ book/27.09.22.(78)/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+if ('3' == 3) {
+    console.log('true 3');
+} else {
+    console.log('nope nope ');
+}
+
+let test1 = '1233';
+let test2 = 123;
+
+if (test1 == test2) {
+    console.log('"123" == 123++++');
+} else {
+    console.log('"123 != 123----');
+}
