f42b8f5197d876feb72847f2b5421c85af3754e2
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-27 17:39:41 +0300

    ex80

---
+++ book/27.09.22.(80)/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+if (3 != 3) {
+    console.log('true eeeeee');
+} else {
+    console.log('no[e no[e');
+}
+
+if ('3' != 3) {
+    console.log('trueeeeee');
+} else {
+    console.log('false ');
+}
+
+if (3 !== 3) {
+    console.log('trueeeeeee');
+} else {
+    console.log('falseeeeee');
+}
+
+if ('3' !== 3) {
+    console.log('true eeeee');
+} else {
+    console.log('false noooo');
+}
+
+let test1 = '3';
+let test2 = '3';
+
+if (test1 != test2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+if (test1 !== test2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+test1 = 3;
+test2 = '3';
+
+if (test1 != test2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+if (test1 !== test2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+test1 = 3;
+test2 = 2;
+
+if (test1 !== test2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
