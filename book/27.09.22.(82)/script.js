3a14b8e3b916e65fa4f61856217fb8afcf7586f0
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-27 18:23:26 +0300

    ex82

---
+++ book/27.09.22.(82)/script.js
@@ -0,0 +1,79 @@
+'use strict';
+
+let num1 = 10;
+let num2 = -5;
+
+if (num1 > 0 || num2 > 0) {
+    console.log('++++ yes');
+} else {
+    console.log('---- false');
+}
+
+num1 = -10;
+num2 = -10;
+
+if (num1 >= 0 || num2 >= 0) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+num1 = 0;
+num2 = 0;
+
+if (num1 >= 0 || num2 >= 0) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+num1 = 0;
+num2 = 5;
+
+if (num1 >= 0 || num2 >= 0) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+num1 = 5;
+num2 = 5;
+
+if (num1 >= 0 || num2 >= 0) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+num1 = -5;
+num2 = 15;
+
+if (num1 >= 0 || num2 >= 0) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+let num = 1;
+
+if (num == 0 || num == 1) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+num = 2;
+
+if (num == 0 || num == 1) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+num = 2;
+
+if (num == 0 || num == 1 || num == 2) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
