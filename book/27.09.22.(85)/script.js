c909f490c525aef84096b9f1e78d978605eb937b
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-28 12:01:00 +0300

    ex85

---
+++ book/27.09.22.(85)/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+if (num > 0 && num < 5) {
+    console.log('++++');
+} else {
+    console.log('____----');
+}
+
+if (num <= 0 || num >= 5) {
+    console.log('sjldfja ');
+} else {
+    console.log('nope');
+}
+
+if (!(num > 0 && num < 5)) {
+    console.log('yes');
+} else {
+    console.log('nupe');
+}
+
+if (num1 >= 0 || num2 <= 10) {
+    console.log('yes');
+} else {
+    console.log('nope');
+}
+
+if (!(num1 >= 0 || num2 <= 10)) {
+    console.log('yes');
+} else {
+    console.log('nope ');
+}
