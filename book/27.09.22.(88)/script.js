e7e8e67b415d9d449a3dd1e9799d05e6cf306993
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-28 12:19:18 +0300

    ex88

---
+++ book/27.09.22.(88)/script.js
@@ -0,0 +1,20 @@
+'use strict';
+let test = 1;
+
+if (test === true) {
+    console.log('++++');
+} else {
+    console.log('nope');
+}
+
+if (test) {
+    console.log('_++++');
+} else {
+    console.log('nope');
+}
+
+if (test) {
+    console.log('diama afaf yeas its work');
+} else {
+    console.log('nope no[pe ');
+}
