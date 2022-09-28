5f55c0b7e20eeeb8cd125798803b6ba357e2142e
dgold <dgold@MacBook-Pro-DGold.local>
2022-09-28 13:32:32 +0300

    ex90

---
+++ book/27.09.22.(90)/script.js
@@ -0,0 +1,94 @@
+'use strict';
+
+let test = 3;
+
+if (test) {
+    console.log('++++')
+} else {
+    console.log('-----')
+}
+
+test = 3;
+
+if (Boolean(test) === true) {
+    console.log('++dfsf++')
+} else {
+    console.log('____') 
+}
+
+if (test) {
+    console.log('fskfjsd')
+}else {
+    console.log('dlkfsjldfkj-----')
+}
+
+test = 'abc';
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+test = '';
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+test = 3 * 'abc';
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+test = null;
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+test = false;
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+test;
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+test = 0;
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+test = '0';
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+test = -1;
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
