16833385c5577a7f098d6cee338c3b70d2dbca71
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-29 22:50:23 +0300

    ex93

---
+++ book/29.09.22.(93)/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let test = 0;
+
+if (test === 0) {
+    console.log('yes');
+} else {
+    console.log('nope');
+}
+
+if (test === 0) console.log('yes');
+else console.log('nope');
+
+if (test > 0) console.log('yes');
+else console.log('nope');
+
+if (test > 0) console.log('yeeees');
+
+test = 3;
+
+if (test > 0) console.log('test');
+console.log('hheheheeh');
