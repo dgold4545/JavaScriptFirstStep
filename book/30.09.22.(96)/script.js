b182810703cb972e26233b96cae655fb61328a20
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-30 06:51:02 +0300

    ex96

---
+++ book/30.09.22.(96)/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let num = 1;
+
+switch (num) {
+    case 1:
+        console.log('value1');
+        break;
+    case 2:
+        console.log('value2');
+        break;
+    case 3:
+        console.log('value3');
+        break;
+    default:
+        console.log('incorrect value');
+}
+
+let lang = 'ru';
+
+switch (lang) {
+    case 'ru':
+        console.log('go go dady');
+        break;
+    case 'en':
+        console.log('go go mister');
+        break;
+    case 'de':
+        console.log('go go unger');
+        break;
+    default:
+        console.log('we dont speak in Americano!!!');
+}
