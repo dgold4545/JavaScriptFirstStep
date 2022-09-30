640d4eae29f54762fe721cf26177c87a00116246
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-30 07:48:51 +0300

    ex101

---
+++ book/30.09.22.(101)/script.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// let age = 117;
+// let adult;
+// if (age >= 18) {
+//     adult = true;
+// } else {
+//     adult = false;
+// }
+
+// console.log(adult);
+
+// let res = 1;
+
+// if (true) {
+//     let res = 2;
+//     console.log(res);
+// }
+// console.log(res);
+
+// let age = 117;
+// let adult;
+
+// if (age >= 18) {
+//     adult = true;
+// } else {
+//     adult = false;
+// }
+
+// console.log(adult);
+
+// let age = 17;
+// let res;
+
+// if (age >= 18) {
+//     if (age <= 23) {
+//         res = 'от 18 до 23';
+//     } else {
+//         res = 'больше 23';
+//     }
+// } else {
+//     res = 'меньше 18';
+// }
+
+// console.log(res);
+
+let age = 19;
+let res;
+
+if (age >= 18) {
+    if (age <= 23) {
+        res = 'от 18 до 23';
+    } else {
+        res = 'больше 23';
+    }
+} else {
+    res = 'меньше 18';
+}
+
+console.log(res);
