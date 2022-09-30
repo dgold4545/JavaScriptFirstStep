74a0f6be9b751a9cb70a4ed4c04af0fb563c075a
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-30 08:06:56 +0300

    ex104

---
+++ book/30.09.22.(104)/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let str = '12345';
+
+if (str.length >= 3) {
+    console.log('!');
+}
+
+let arr = [12, 32, 323, 41341, 324];
+if (arr.length == 5) {
+    console.log(arr.length);
+    let dima = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+    console.log(dima);
+}
