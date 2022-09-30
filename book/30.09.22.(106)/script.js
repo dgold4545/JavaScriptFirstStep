06afe6cb05dbc2e7fede5a4becd48c35f2a14fd2
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-09-30 08:36:48 +0300

    ex106

---
+++ book/30.09.22.(106)/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let num = 12345;
+
+let str = 12342354312;
+let str1 = String(str);
+console.log(str1);
+
+if (str1[str1.length - 1] == 0) {
+    console.log('yes its  a zero');
+} else {
+    console.log('no[e');
+}
+
+let dima = 124514251212;
+let dima1 = String(dima);
+
+if (
+    dima1[dima1.length - 1] == 0 ||
+    dima1[dima1.length - 1] == 2 ||
+    dima1[dima1.length - 1] == 4 ||
+    dima1[dima1.length - 1] == 6 ||
+    dima1[dima1.length - 1] == 8
+) {
+    console.log(dima);
+}
