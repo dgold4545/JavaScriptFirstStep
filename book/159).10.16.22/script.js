e938ff12cb73012429f9723feaa5994a7c75b5b9
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-10-16 08:01:00 +0300

    ex159

---
+++ book/159).10.16.22/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function func(num) {
+    console.log(num ** 8);
+}
+
+let param = 2;
+
+func(param);
+
+let param1 = 11;
+let param2 = 2;
+let param3 = 3;
+
+function sumNumb(num1, num2, num3) {
+    console.log(num1 + num2 + num3);
+}
+
+sumNumb(param1, param2, param3);
