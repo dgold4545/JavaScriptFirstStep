4332d3171571c05a7a2cdee94d876b1447a3a787
Dmytro Holovskyi <80936904545dgold@gmail.com>
2022-10-16 07:53:07 +0300

    ex158

---
+++ book/158).10.16.22/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function func(num1, num2) {
+    console.log(num1 + num2);
+}
+
+func(12, 12);
+
+function thee(num1, num2, num3) {
+    console.log(num1 + num3 + num2);
+}
+
+thee(11, 11, 130);
